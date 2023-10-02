9fdc0c9023fa230bc75251170bea8644d419e526
Kunwar Vabhav Mishra <76851162+kvm22@users.noreply.github.com>
2023-10-02 10:09:17 +0000

    learnt about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const uid= "20bcs9361"
+let studentName="Kunwar Vabhav Mishra"
+var emailId="vabhavmishra69@gmail.com"
+studentCity="Lucknow"
+
+studentName="xlposiv"
+emailId="xplosiv@gmail.com"
+studentCity="Amethi"
+
+
+// uid="20bcs9369"
+
+/* 
+Prefer not to use var
+beacuse of block scope and functional scope 
+*/
+
+console.log(uid)
+console.table([uid,studentName,emailId,studentCity])
