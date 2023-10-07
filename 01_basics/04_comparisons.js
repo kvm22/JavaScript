0ad8b4c479b74e4cd970ef035f4495108929165b
Kunwar Vabhav Mishra <76851162+kvm22@users.noreply.github.com>
2023-10-07 06:08:13 +0000

    comarisons learnt

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,23 @@
+// Normal comparisons
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// Here it is liitle tricky cause of diffrent working of == and >=,<= and here we ARE comparing different datatypes like in first one string and number so sometimes it dosn't give predicatable result as null first converted into 0 then it  check it therefore so.
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+// for strict comparion -> checks if datatype is same then compare
+console.log("2" === 2);
