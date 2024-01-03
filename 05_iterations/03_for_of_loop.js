3e450343d3417cf19e829bd09e678b15861a2908
Kunwar Vabhav Mishra <76851162+kvm22@users.noreply.github.com>
2024-01-03 11:07:42 +0000

    for_of loop

---
+++ 05_iterations/03_for_of_loop.js
@@ -0,0 +1,48 @@
+// for of
+
+// we are using for of loop for iteration in (arrays, sets, maps, strings etc)
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+
+
+// by using sqaure bracket we can iterate over key and value
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+
+// The for...of loop cannot be used to iterate over an object
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
